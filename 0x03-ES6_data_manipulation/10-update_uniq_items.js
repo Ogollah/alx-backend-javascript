5c52eebbc3fb2251d85fda208c06b5a87281c261
Ogollah <kogollah2011@gmail.com>
2024-01-09 15:25:07 +0300

    10. More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+export default function updateUniqueItems(items) {
+  if (!(items instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of items.entries()) {
+    if (value === 1) {
+      items.set(key, 100);
+    }
+  }
+
+  return items;
+}
