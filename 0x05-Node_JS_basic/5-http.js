713fd01de397207c8f280f4276520d8fa4d94cfb
Ogollah <kogollah2011@gmail.com>
2024-02-27 19:22:38 +0300

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,3 @@
-#!/usr/bin/node
-
 const http = require('http');
 const students = require('./3-read_file_async');
 const hostname = '127.0.0.1';
@@ -14,10 +12,8 @@ const app = http.createServer((req, res) => {
     res.write('This is the list of our students\n');
     students(process.argv[2]).then((data) => {
       res.write(`Number of students: ${data.students.length}\n`);
-      res.write(`Number of students in CS: ${data.csStudents.length}.
-        List: ${data.csStudents.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${data.sweStudents.length}.
-        List: ${data.sweStudents.join(', ')}`);
+      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
       res.end();
     }).catch((err) => res.end(err.message));
   }
