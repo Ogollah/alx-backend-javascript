ccfe25e53d4273af6b16ef5cef194f27723bf073
Ogollah <kogollah2011@gmail.com>
2024-02-27 19:09:52 +0300

    NodeJs basic

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+class AppController {
+    static getHomepage(request, response) {
+      response.send(200, 'Hello Holberton School!');
+    }
+  }
+
+  export default AppController;
---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+import express from 'express';
+
+const app = express();
+const indexRouter = require('./routes/index');
+const port = 1245;
+
+app.listen(port);
+app.use('/', indexRouter);
+app.use('/students', indexRouter);
+app.use('/students/:major', indexRouter);
+
+export default app;
