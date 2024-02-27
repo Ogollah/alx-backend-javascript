35441457a1de3f5d4063b50a06b44071dba7026a
Ogollah <kogollah2011@gmail.com>
2024-02-27 19:39:57 +0300

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,33 +1,44 @@
-#!/usr/bin/node
-
 const express = require('express');
-const students = require('./3-read_file_async');
-const app = express();
-const hostname = '127.0.0.1';
-const port = 1245;
+const fs = require('fs');
+const path = require('path');
 
+const app = express();
+
+// Define port
+const PORT = 1245;
+
+// Route for "/"
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  await students(process.argv[2]).then((data) => {
-    res.write(`Number of students: ${data.students.length}\n`);
-    res.write(`Number of students in CS:
-      ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-    res.write(`Number of students in SWE:
-      ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-  }).catch((err) => res.write(err.message))
-    .finally(() => {
-      res.end();
+// Route for "/students"
+app.get('/students', (req, res) => {
+  const dbFilePath = req.query.db || '';
+  const filePath = path.join(__dirname, dbFilePath);
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error reading the file');
+    }
+
+    // Split the file content by newline
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+
+    // Prepare response
+    let response = 'This is the list of our students\n';
+    lines.forEach(line => {
+      const [name, age, field] = line.split(',');
+      response += `Name: ${name}, Age: ${age}, Field: ${field}\n`;
     });
+
+    res.type('text').send(response);
+  });
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
