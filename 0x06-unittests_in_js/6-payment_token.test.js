22f0863e71a998c06fa8ab405dd76fc2c9e744ad
Ogollah <kogollah2011@gmail.com>
2024-02-29 23:37:06 +0300

    Unittests in JS

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const { expect } = require('chai');
+
+const getPaymentTokenFromApi = require('./6-payment_token');
+
+describe('getPaymentTokenFromApi', () => {
+  it('checks output of getPaymentTokenFromApi with true as success', (done) => {
+    getPaymentTokenFromApi(true)
+      .then((res) => {
+        expect(res).to.include({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
